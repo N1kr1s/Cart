b826c9d9aa1a5d5948e67aa57ce7b7466bf760c3
Nio <nikris@mail.ru>
2024-11-11 15:00:14 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
+import { useGlobalContext } from './context'
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  )
+}
+
+export default App
